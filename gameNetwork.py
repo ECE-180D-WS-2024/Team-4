fea2a098ea060071b2d5fc9047f088de3434eed8
christophejc <christophejc24@g.ucla.edu>
2024-06-04 18:54:40 -0700

    socket play

--- gameNetwork.py
+++ gameNetwork.py
@@ -1,14 +1,22 @@
 import socket
 
 
+#Helper Funcitons
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1]), int(str[2]), int(str[3]), bool(str[4]), bool(str[5]), bool(str[6])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[2]) + "," + str(tup[3]) + "," + str(tup[4]) + "," +  str(tup[5]) + "," + str(tup[6])
+
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "172.16.1.36"
+        self.server = "172.30.51.245"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.pos = self.connect()
-        print(self.pos)
+        #print(self.pos)
         
 
     def connect(self):
@@ -32,6 +40,4 @@ class Network:
             print(e)
 
 
-#n = Network()
-#print(n.send("hello"))
-#print(n.send("working"))
+
--- gameServer.py
+++ gameServer.py
@@ -3,48 +3,79 @@ import time
 from _thread import *
 import sys
 
-server = "172.16.1.36"
+server = "172.30.51.245"
 port = 5555
+currentPlayer = 0
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     s.bind((server, port))
 except socket.error as e:
-    str(e)
+    print(str(e))
+    sys.exit()
 
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
+#def get_keypress():
+#    keys = event.getKeys()
+#    if keys and keys[0] == 'Esc':
+#        print("Exiting...")
+#        exit()
+#    elif keys:
+#        return keys[0]
+#    else:
+#        return None
 
-def threaded_client(conn,):
-    conn.send(str.encode("Connected"))
+#Helper Funcitons
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1]), int(str[2]), int(str[3]), int(str[4]), int(str[5]), int(str[6]), int(str[7]), int(str[8]), int(str[9]), int(str[10])
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[2]) + "," + str(tup[3]) + "," + str(tup[4]) + "," +  str(tup[5]) + "," + str(tup[6]) + "," + str(tup[7]) + "," +  str(tup[8]) + "," + str(tup[9]) + "," + str(tup[10])
+
+players = [(0,0,0,0 , 0,0,0,0 , 0, 0, 0), (0,0,0,0 , 0,0,0,0 , 0, 0, 0)]
+
+def threaded_client(conn, currentP):
+    conn.send(str.encode(make_pos(players[currentP])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode())
+            #reply = data.decode("utf-8")
+
+            players[currentP] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
-                print("Received: ", reply)
-                print("Sending : ", reply)
+                if currentP== 1:
+                    reply = players[0]
+                else:
+                    reply = players[1]
+                #print("Received: ", data)
+                #print("Sending : ", reply)
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
         except:
             break
 
     print("Lost connection")
+    global currentPlayer
+    if(currentPlayer != 0):
+        currentPlayer -= 1
     conn.close()
+
 
 
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-
-    start_new_thread(threaded_client, (conn,))
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
+#    get_keypress()
 
 
 #######################################
